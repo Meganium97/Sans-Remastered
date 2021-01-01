c7dea011460de8dad6241945710d2fe49ab7d069
Dire <73264332+Meganium97@users.noreply.github.com>
2021-01-01 13:16:48 -0600

    Everything.

--- sans.py
+++ sans.py
@@ -43,3 +43,82 @@ async def on_ready():
 async def update(ctx):
     await ctx.send("Updating code. The bot will be down for a few seconds, if this doesnt break the code.")
     subprocess.run(['bash', home_path + '/update.sh'])
+
+@bot.command()
+@commands.has_any_role('Owner', 'Staff', 'Admin')
+async def ban(ctx, member: discord.User = None, reason="[no reason specified]"):
+    '''does what it says on the tin'''
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself.")
+        return
+    reasonraw = ctx.message.content[28:]
+    message = f"You have been banned from {ctx.guild.name} for the reason {reasonraw}"
+    await ctx.guild.ban(member, reason=reasonraw, delete_message_days=0)
+    await ctx.channel.send(f"{member} has been b&. 👍")
+    await member.send(message)
+    
+@bot.command()
+@commands.has_any_role('Owner', 'Staff', 'Admin', 'Helper')
+async def kick(ctx, member: discord.Member, *, reason=0):
+    '''does what it says on the tin'''
+    await member.kick(reason=reason, delete_message_days=0)
+    send = f"user {member.name} has been kicked."
+    await ctx.send(send)
+
+@bot.command()
+@commands.has_any_role('Owner', 'Staff', 'Admin', 'Helper', 'Sans Contributor')
+async def say(ctx, message):
+    '''does what it says on the tin (Sans Contributors+)'''
+    await ctx.message.delete()
+    await ctx.send(ctx.message.content[5:])
+    
+@bot.command(aliases=["pfp"])
+async def profile(ctx, user: discord.User):
+    '''Fetch a user's profile icon'''
+    await ctx.send(f"Profile image for user: {user.name}")
+    pfp = user.avatar_url
+    await ctx.send(pfp)
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f':ping_pong: Pong! Latency is {int(bot.latency * 1000)} ms!')
+    
+@bot.group()
+async def contributors(ctx):
+    '''Links contributors to the code'''
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Which one? Current or old?')
+
+@contributors.command()
+async def old(ctx):
+    '''Lists the old Sans contributors'''
+    embed = discord.Embed(
+        title="List of old Sans contributors",
+        description=cleandoc("""
+            Lazr1026 - Creator and programmer
+            476MHz (Radeon) - Programmer
+            Uwuham - Not very much, he makes commits sometimes
+            Techmuse - PR'd useful shit
+            Gnome! - Cleaned everything up massively
+            ItsPizzaTime1501 - Helped with proper licensing
+            bleck9999 - Fixed Gnomes mistakes
+            Maretu (ray) - Fixed our terrible grammar
+            Meganium97 (Dire) - Idk, what are you asking me for?
+            Glazed_Belmont - Fixed the paths not being universal
+        """)
+    )
+    
+    await ctx.send(embed=embed)
+
+@contributors.command()
+async def current(ctx):
+    '''Lists currect Sans contributors'''
+    embed = discord.Embed(
+        title="List of current Sans contributors",
+        description=cleandoc("""
+            Meganium97 (Dire) - Current leading coder
+        """)
+    )
+    
+    await ctx.send(embed=embed)
+bot.run(token)
